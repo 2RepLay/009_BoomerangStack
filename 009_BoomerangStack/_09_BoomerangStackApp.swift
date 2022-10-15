55c3a1fcc22b41a0b94e6d9c382430e48c1f0a2c
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-15 17:36:31 +0300

    Initial Commit

---
+++ 009_BoomerangStack/_09_BoomerangStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  _09_BoomerangStackApp.swift
+//  009_BoomerangStack
+//
+//  Created by nikita on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _09_BoomerangStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
